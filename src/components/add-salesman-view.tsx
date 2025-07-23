cf17d6231a7327f74fc09e808674e2adc8cc0f3e
Manuja Niroshan <manujaniroshan5@gmail.com>
2025-07-23 06:01:37 +0000

    Runtime Error
    Error: React is not defined
    
    src/components/add-salesman

--- src/components/add-salesman-view.tsx
+++ src/components/add-salesman-view.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
